10609d692b32f4b58499baa009682f6172cd3aad
Owen Synge <os@nect.com>
2021-05-04 14:50:40 +0200

    apply gofmt

--- healthd.go
+++ healthd.go
@@ -1,124 +1,124 @@
 package main
 
 import (
-    "context"
-    "encoding/json"
-    "fmt"
-    "io/ioutil"
-    "net/http"
-    "time"
+	"context"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"time"
 
-    "github.com/sirupsen/logrus"
-    etcdClient "go.etcd.io/etcd/client"
+	"github.com/sirupsen/logrus"
+	etcdClient "go.etcd.io/etcd/client"
 )
 
 // Healthd is the service used for receiveing status updates and persisting them in etcd
 type Healthd struct {
-    etcds  StringSlice
-    client etcdClient.Client
+	etcds  StringSlice
+	client etcdClient.Client
 }
 
 // NewHealthd creates a new service for receiving status updates and persisting them to etcd
 func NewHealthd(etcds StringSlice) (*Healthd, error) {
-    h := &Healthd{
-        etcds: etcds,
-    }
+	h := &Healthd{
+		etcds: etcds,
+	}
 
-    var err error
-    h.client, err = etcdClient.New(etcdClient.Config{
-        Endpoints: etcds,
-    })
-    if err != nil {
-        return nil, fmt.Errorf("couldn't connect to etcds `%#v`, see: %v", etcds, err)
-    }
+	var err error
+	h.client, err = etcdClient.New(etcdClient.Config{
+		Endpoints: etcds,
+	})
+	if err != nil {
+		return nil, fmt.Errorf("couldn't connect to etcds `%#v`, see: %v", etcds, err)
+	}
 
-    return h, nil
+	return h, nil
 }
 
 // StatusUpdate represents the struct which should be passed to etcd
 type StatusUpdate struct {
-    Status []interface{}
-    Time   int64
+	Status []interface{}
+	Time   int64
 }
 
 // GETMonitorsHandler is the endpoint for reading out the endpoints which should be monitored
 func (h *Healthd) GETMonitorsHandler(w http.ResponseWriter, r *http.Request) {
-    kapi := etcdClient.NewKeysAPI(h.client)
-    ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-    resp, err := kapi.Get(ctx, "monitors", nil)
-    cancel()
-    if err != nil {
-        errStr := fmt.Sprintf("couldn't retrieve monitors from etcd, see: %v", err)
-        logrus.Error(errStr)
-        http.Error(w, errStr, http.StatusInternalServerError)
-        return
-    }
+	kapi := etcdClient.NewKeysAPI(h.client)
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	resp, err := kapi.Get(ctx, "monitors", nil)
+	cancel()
+	if err != nil {
+		errStr := fmt.Sprintf("couldn't retrieve monitors from etcd, see: %v", err)
+		logrus.Error(errStr)
+		http.Error(w, errStr, http.StatusInternalServerError)
+		return
+	}
 
-    if resp == nil || resp.Node == nil {
-        errStr := "missing node in response from etcd"
-        logrus.Error(errStr)
-        http.Error(w, errStr, http.StatusInternalServerError)
-        return
-    }
+	if resp == nil || resp.Node == nil {
+		errStr := "missing node in response from etcd"
+		logrus.Error(errStr)
+		http.Error(w, errStr, http.StatusInternalServerError)
+		return
+	}
 
-    val := resp.Node.Value
-    w.WriteHeader(http.StatusOK)
-    w.Write([]byte(val))
+	val := resp.Node.Value
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(val))
 }
 
 // POSTStatusHandler is the handlerFunc used as endpoint for receiving status updates
 func (h *Healthd) POSTStatusHandler(w http.ResponseWriter, r *http.Request) {
-    agentName := r.Header.Get("X-Agent-Name")
+	agentName := r.Header.Get("X-Agent-Name")
 
-    if r.ContentLength <= 0 || r.ContentLength > 1000000 {
-        errStr := fmt.Sprintf("invalid content length, expected some bytes with a max of 1mb, got `%d`", r.ContentLength)
-        logrus.Error(errStr)
-        http.Error(w, errStr, http.StatusBadRequest)
-        return
-    }
+	if r.ContentLength <= 0 || r.ContentLength > 1000000 {
+		errStr := fmt.Sprintf("invalid content length, expected some bytes with a max of 1mb, got `%d`", r.ContentLength)
+		logrus.Error(errStr)
+		http.Error(w, errStr, http.StatusBadRequest)
+		return
+	}
 
-    body, err := ioutil.ReadAll(r.Body)
-    if err != nil {
-        errStr := fmt.Sprintf("couldn't read request body, see: %v", err)
-        logrus.Error(errStr)
-        http.Error(w, errStr, http.StatusBadRequest)
-        return
-    }
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		errStr := fmt.Sprintf("couldn't read request body, see: %v", err)
+		logrus.Error(errStr)
+		http.Error(w, errStr, http.StatusBadRequest)
+		return
+	}
 
-    var data []interface{}
-    err = json.Unmarshal(body, &data)
-    if err != nil {
-        errStr := fmt.Sprintf("couldn't parse `%s` as json, see: %v", string(body), err)
-        logrus.Error(errStr)
-        http.Error(w, errStr, http.StatusBadRequest)
-        return
-    }
+	var data []interface{}
+	err = json.Unmarshal(body, &data)
+	if err != nil {
+		errStr := fmt.Sprintf("couldn't parse `%s` as json, see: %v", string(body), err)
+		logrus.Error(errStr)
+		http.Error(w, errStr, http.StatusBadRequest)
+		return
+	}
 
-    update := &StatusUpdate{
-        Status: data,
-        Time:   time.Now().Unix(),
-    }
+	update := &StatusUpdate{
+		Status: data,
+		Time:   time.Now().Unix(),
+	}
 
-    body, err = json.Marshal(update)
-    if err != nil {
-        errStr := fmt.Sprintf("couldn't serialize `%s` as json, see: %v", string(body), err)
-        logrus.Error(errStr)
-        http.Error(w, errStr, http.StatusInternalServerError)
-        return
-    }
+	body, err = json.Marshal(update)
+	if err != nil {
+		errStr := fmt.Sprintf("couldn't serialize `%s` as json, see: %v", string(body), err)
+		logrus.Error(errStr)
+		http.Error(w, errStr, http.StatusInternalServerError)
+		return
+	}
 
-    kapi := etcdClient.NewKeysAPI(h.client)
-    ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-    _, err = kapi.Set(ctx, agentName, string(body), nil)
-    cancel()
-    if err != nil {
-        errStr := fmt.Sprintf("couldn't persist status update to etcd, see: %v", err)
-        logrus.Error(errStr)
-        http.Error(w, errStr, http.StatusInternalServerError)
-    }
+	kapi := etcdClient.NewKeysAPI(h.client)
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	_, err = kapi.Set(ctx, agentName, string(body), nil)
+	cancel()
+	if err != nil {
+		errStr := fmt.Sprintf("couldn't persist status update to etcd, see: %v", err)
+		logrus.Error(errStr)
+		http.Error(w, errStr, http.StatusInternalServerError)
+	}
 
-    w.WriteHeader(http.StatusOK)
-    logrus.Infof("Updated state for agent `%s` to `%#v`", agentName, string(body))
+	w.WriteHeader(http.StatusOK)
+	logrus.Infof("Updated state for agent `%s` to `%#v`", agentName, string(body))
 
-    return
+	return
 }
--- main.go
+++ main.go
@@ -1,16 +1,16 @@
 package main
 
 import (
-    "crypto/tls"
-    "crypto/x509"
-    "flag"
-    "fmt"
-    "io/ioutil"
-    "net/http"
-    "strings"
-    "time"
+	"crypto/tls"
+	"crypto/x509"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"strings"
+	"time"
 
-    "github.com/sirupsen/logrus"
+	"github.com/sirupsen/logrus"
 )
 
 // APPVERSION contains the version of the tool, injected by make
@@ -21,131 +21,131 @@ type StringSlice []string
 
 // String returns a string representation of the current string slice.
 func (i *StringSlice) String() string {
-    return strings.Join(*i, " ")
+	return strings.Join(*i, " ")
 }
 
 // Set appends a entry to the string slice (used for flags)
 func (i *StringSlice) Set(value string) error {
-    *i = append(*i, value)
-    return nil
+	*i = append(*i, value)
+	return nil
 }
 
 func main() {
-    logrus.Infof("healthd v%s", APPVERSION)
+	logrus.Infof("healthd v%s", APPVERSION)
 
-    var caPath string
-    var crtPath string
-    var keyPath string
-    var etcds StringSlice
-    var httpsPort int
+	var caPath string
+	var crtPath string
+	var keyPath string
+	var etcds StringSlice
+	var httpsPort int
 
-    flag.Var(&etcds, "etcd", "etcd endpoint where status should be persisted. Multiple can be given, e.g.: -etcd localhost:2379 -etcd localhost:22379")
-    flag.StringVar(&caPath, "ca", "", "path to the ca.crt")
-    flag.StringVar(&crtPath, "crt", "", "path to the client.crt")
-    flag.StringVar(&keyPath, "key", "", "path to the client.key")
-    flag.IntVar(&httpsPort, "port", 443, "port on which the https server should listen")
-    flag.Parse()
+	flag.Var(&etcds, "etcd", "etcd endpoint where status should be persisted. Multiple can be given, e.g.: -etcd localhost:2379 -etcd localhost:22379")
+	flag.StringVar(&caPath, "ca", "", "path to the ca.crt")
+	flag.StringVar(&crtPath, "crt", "", "path to the client.crt")
+	flag.StringVar(&keyPath, "key", "", "path to the client.key")
+	flag.IntVar(&httpsPort, "port", 443, "port on which the https server should listen")
+	flag.Parse()
 
-    if len(etcds) == 0 {
-        logrus.Fatal("no etcds given, pass them using -etcd")
-    }
+	if len(etcds) == 0 {
+		logrus.Fatal("no etcds given, pass them using -etcd")
+	}
 
-    if caPath == "" {
-        logrus.Fatal("no ca certificate given, pass it using -ca")
-    }
+	if caPath == "" {
+		logrus.Fatal("no ca certificate given, pass it using -ca")
+	}
 
-    if crtPath == "" {
-        logrus.Fatal("no client certificate given, pass it using -crt")
-    }
+	if crtPath == "" {
+		logrus.Fatal("no client certificate given, pass it using -crt")
+	}
 
-    if keyPath == "" {
-        logrus.Fatal("no client key given, pass it using -key")
-    }
+	if keyPath == "" {
+		logrus.Fatal("no client key given, pass it using -key")
+	}
 
-    if httpsPort <= 0 {
-        logrus.Fatalf("Invalid https port `%d` specified, pass using -port", httpsPort)
-    }
+	if httpsPort <= 0 {
+		logrus.Fatalf("Invalid https port `%d` specified, pass using -port", httpsPort)
+	}
 
-    // Create a CA certificate pool and add cert.pem to it
-    caCert, err := ioutil.ReadFile(caPath)
-    if err != nil {
-        logrus.Fatalf("couldn't read `%s`, see: %v", caPath, err)
-    }
+	// Create a CA certificate pool and add cert.pem to it
+	caCert, err := ioutil.ReadFile(caPath)
+	if err != nil {
+		logrus.Fatalf("couldn't read `%s`, see: %v", caPath, err)
+	}
 
-    caCertPool := x509.NewCertPool()
-    caCertPool.AppendCertsFromPEM(caCert)
+	caCertPool := x509.NewCertPool()
+	caCertPool.AppendCertsFromPEM(caCert)
 
-    // Create the TLS Config with the CA pool and enable Client certificate validation
-    tlsConfig := &tls.Config{
-        ClientCAs:  caCertPool,
-        ClientAuth: tls.RequireAndVerifyClientCert,
-    }
-    tlsConfig.BuildNameToCertificate()
+	// Create the TLS Config with the CA pool and enable Client certificate validation
+	tlsConfig := &tls.Config{
+		ClientCAs:  caCertPool,
+		ClientAuth: tls.RequireAndVerifyClientCert,
+	}
+	tlsConfig.BuildNameToCertificate()
 
-    h, err := NewHealthd(etcds)
-    if err != nil {
-        logrus.Fatalf("couldn't create healthd service, see: %v", err)
-    }
+	h, err := NewHealthd(etcds)
+	if err != nil {
+		logrus.Fatalf("couldn't create healthd service, see: %v", err)
+	}
 
-    handler := func(w http.ResponseWriter, r *http.Request) {
-        agentName := r.Header.Get("X-Agent-Name")
-        if agentName == "" {
-            errStr := "request missing X-Agent-Name header, ignoring."
-            logrus.Error(errStr)
-            http.Error(w, errStr, http.StatusBadRequest)
-            return
-        }
+	handler := func(w http.ResponseWriter, r *http.Request) {
+		agentName := r.Header.Get("X-Agent-Name")
+		if agentName == "" {
+			errStr := "request missing X-Agent-Name header, ignoring."
+			logrus.Error(errStr)
+			http.Error(w, errStr, http.StatusBadRequest)
+			return
+		}
 
-        if r.TLS == nil || len(r.TLS.PeerCertificates) == 0 {
-            errStr := "missing tls informations"
-            logrus.Error(errStr)
-            http.Error(w, errStr, http.StatusBadRequest)
-            return
-        }
+		if r.TLS == nil || len(r.TLS.PeerCertificates) == 0 {
+			errStr := "missing tls informations"
+			logrus.Error(errStr)
+			http.Error(w, errStr, http.StatusBadRequest)
+			return
+		}
 
-        foundName := false
-        if r.TLS.PeerCertificates[0].Subject.CommonName == agentName {
-            foundName = true
-        } else {
-            for _, n := range r.TLS.PeerCertificates[0].DNSNames {
-                if n == agentName {
-                    foundName = true
-                    break
-                }
-            }
-        }
+		foundName := false
+		if r.TLS.PeerCertificates[0].Subject.CommonName == agentName {
+			foundName = true
+		} else {
+			for _, n := range r.TLS.PeerCertificates[0].DNSNames {
+				if n == agentName {
+					foundName = true
+					break
+				}
+			}
+		}
 
-        if !foundName {
-            errStr := fmt.Sprintf("invalid agent name `%s` (not in CN or SANs)", agentName)
-            logrus.Error(errStr)
-            http.Error(w, errStr, http.StatusForbidden)
-            return
-        }
+		if !foundName {
+			errStr := fmt.Sprintf("invalid agent name `%s` (not in CN or SANs)", agentName)
+			logrus.Error(errStr)
+			http.Error(w, errStr, http.StatusForbidden)
+			return
+		}
 
-        if r.Method == http.MethodGet {
-            h.GETMonitorsHandler(w, r)
-        } else if r.Method == http.MethodPost {
-            h.POSTStatusHandler(w, r)
-        } else {
-            errStr := fmt.Sprintf("invalid method `%s` expected GET or POST", r.Method)
-            logrus.Error(errStr)
-            http.Error(w, errStr, http.StatusBadRequest)
-        }
-    }
+		if r.Method == http.MethodGet {
+			h.GETMonitorsHandler(w, r)
+		} else if r.Method == http.MethodPost {
+			h.POSTStatusHandler(w, r)
+		} else {
+			errStr := fmt.Sprintf("invalid method `%s` expected GET or POST", r.Method)
+			logrus.Error(errStr)
+			http.Error(w, errStr, http.StatusBadRequest)
+		}
+	}
 
-    // Set up a /hello resource handler
-    http.HandleFunc("/", handler)
+	// Set up a /hello resource handler
+	http.HandleFunc("/", handler)
 
-    // Create a Server instance to listen with the TLS config
-    server := &http.Server{
-        Addr:           fmt.Sprintf(":%d", httpsPort),
-        ReadTimeout:    10 * time.Second,
-        WriteTimeout:   10 * time.Second,
-        MaxHeaderBytes: 1 << 20,
-        TLSConfig:      tlsConfig,
-    }
+	// Create a Server instance to listen with the TLS config
+	server := &http.Server{
+		Addr:           fmt.Sprintf(":%d", httpsPort),
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+		TLSConfig:      tlsConfig,
+	}
 
-    // Listen to HTTPS connections with the server certificate and wait
-    err = server.ListenAndServeTLS(crtPath, keyPath)
-    logrus.Fatalf("received error in https server, see: %v", err)
+	// Listen to HTTPS connections with the server certificate and wait
+	err = server.ListenAndServeTLS(crtPath, keyPath)
+	logrus.Fatalf("received error in https server, see: %v", err)
 }
